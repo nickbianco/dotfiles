854efb3822a333a55d3f8089e0d163dd9ad1b2f5
Nicholas Bianco <nbianco@Nicholass-MBP.attlocal.net>
2024-10-13 21:42:23 -0700

    Add my_config.sh

---
+++ fish/functions/my_config.fish
@@ -0,0 +1,9 @@
+# Source this file at the top of ~/.config/fish/config.fish.
+
+set -U fish_greeting
+
+# fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+# fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
+# fish_add_path /opt/homebrew/opt/openjdk/bin
+
+# source ~/miniconda3/etc/fish/conf.d/conda.fish
