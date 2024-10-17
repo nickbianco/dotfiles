1b935a9ec5483614b7b6794f30f33b02666770e5
nickbianco <nbianco@stanford.edu>
2024-10-16 21:32:55 -0700

    Set LSCOLORS; tmux message color update

--- fish/functions/my_config.fish
+++ fish/functions/my_config.fish
@@ -1,9 +1,5 @@
 # Source this file at the top of ~/.config/fish/config.fish.
 
-set -U fish_greeting
-
-# fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-# fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
-# fish_add_path /opt/homebrew/opt/openjdk/bin
-
-# source ~/miniconda3/etc/fish/conf.d/conda.fish
+set -gx CLICOLORS 1 # OSX only
+set -gx LSCOLORS 'Exfxcxdxbxegedabagacad' # Linux = LS_COLORS?
+set -g fish_greeting
