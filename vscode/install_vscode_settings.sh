6b51dc4d45e74912e0fe787694edb68069662506
nickbianco <nbianco@stanford.edu>
2025-11-07 10:25:01 -0600

    Add install script for vscode workspace settings

---
+++ vscode/install_vscode_settings.sh
@@ -0,0 +1,4 @@
+mkdir .vscode
+mkdir dependencies/.vscode
+cp ~/.dotfiles/vscode/opensim-core/settings.json .vscode/settings.json
+cp ~/.dotfiles/vscode/opensim-core/dependencies/settings.json dependencies/.vscode/settings.json
