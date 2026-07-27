42a7acd873b7503f8cb1beb3400c3b04de62236b
nickbianco <nbianco@stanford.edu>
2026-07-27 14:57:38 -0500

    Add CMakePresets.json files to vscode settings

--- vscode/install_vscode_settings.sh
+++
@@ -1,4 +0,0 @@
-mkdir .vscode
-mkdir dependencies/.vscode
-cp ~/.dotfiles/vscode/opensim-core/settings.json .vscode/settings.json
-cp ~/.dotfiles/vscode/opensim-core/dependencies/settings.json dependencies/.vscode/settings.json
