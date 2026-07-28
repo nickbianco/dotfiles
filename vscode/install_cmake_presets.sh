33a15700b528bb3e8f40a57c552a992641361492
nickbianco <nbianco@stanford.edu>
2026-07-28 10:43:10 -0500

    Fix install_cmake_presets.sh

--- vscode/install_cmake_presets.sh
+++ vscode/install_cmake_presets.sh
@@ -1,2 +1,2 @@
 cp ~/.dotfiles/vscode/opensim-core/CMakeUserPresets.json CMakeUserPresets.json
-cp ~/.dotfiles/vscode/opensim-core/dependencies/CMakeUserPresets.json CMakeUserPresets.json
+cp ~/.dotfiles/vscode/opensim-core/dependencies/CMakeUserPresets.json dependencies/CMakeUserPresets.json
